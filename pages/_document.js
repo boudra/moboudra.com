20319b9ec86f81e4c24829feb529e1d6cc4bf897
Mohamed Boudra <boudra.moha@gmail.com>
2022-11-24 11:25:28 +0200

    feat: add plausible script

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,11 @@ export default class MyDocument extends Document {
             sizes="160x1600"
             href="/favico.png"
           ></link>
+          <script
+            defer
+            data-domain="moboudra.com"
+            src="https://plausible.io/js/script.js"
+          ></script>
         </Head>
         <body className="font-body text-[#023047] bg-[rgb(238,230,228)]">
           <Main />
