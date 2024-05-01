d365544ee76703f70eee0222daa91fe30d99c597
Mohamed Boudra <boudra.moha@gmail.com>
2024-05-01 20:58:17 +0800

    update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,17 +13,12 @@ export async function getStaticProps() {
 export default function Home({ posts }) {
   return (
     <div className="max-w-prose mx-auto px-4">
-      <div className="text-primary pb-6 mb-10 text-lg border-b border-primary/5">
-        <p className="pb-4">Welcome to my site!</p>
-        <p className="pb-4">
-          I’m a technical leader and generalist passionate about the digital
-          product lifecycle, from identifying user needs, to delivering great UX
-          and robust software.
-        </p>
-        <p className="pb-4">
-          I use this space to share the things I make, and write about the
-          things I learn.
-        </p>
+      <div className="text-primary pb-6 mb-10 text-md border-b border-primary/5 font-mono">
+        Hi, I'm Mo, a technical lead and generalist passionate about the digital
+        product lifecycle - from identifying user needs, to delivering robust
+        software with great UX. I've been building things for quite some time,
+        some of which you can find on this site. I also write about things that
+        interest me.
       </div>
       <div className="pb-12">
         <div className="pb-6">
